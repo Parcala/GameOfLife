d5174676cbab70c1cdc71b5c2606d3eb5ab2ecdb
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-08-04 16:53:11 -0400

    Initial Commit

---
+++ GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,115 @@
+using NUnit.Framework;
+using System.Net;
+
+namespace GameOfLife.Tests
+{
+    public class GameOfLifeTests
+    {
+        /*
+        [SetUp]
+        public void Setup()
+        {
+        }
+        */
+
+        public static bool[,] testGridRule1 =
+        {
+            { false, false, true, false, false },
+            { false, false, true, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false }
+        };
+
+        public static bool[,] ExpectedGridRule1 =
+        {
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false }
+        };
+
+        public static bool[,] testGridRule2 =
+        {
+            { true, true, true, true, true },
+            { true, true, true, true, true },
+            { true, true, true, true, true },
+            { true, true, true, true, true },
+            { true, true, true, true, true }
+        };
+
+        public static bool[,] ExpectedGridRule2 =
+        {
+            { true, false, false, false, true },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { true, false, false, false, true }
+        };
+
+        public static bool[,] testGridRule3 =
+        {
+            { false, false, true, false, false },
+            { false, true, false, true, false },
+            { false, true, false, true, false },
+            { false, false, true, false, false },
+            { false, false, false, false, false }
+        };
+
+        public static bool[,] ExpectedGridRule3 =
+        {
+            { false, false, true, false, false },
+            { false, true, false, true, false },
+            { false, true, false, true, false },
+            { false, false, true, false, false },
+            { false, false, false, false, false }
+        };
+
+        public static bool[,] testGridRule4 =
+        {
+            { false, true, true, false, false },
+            { false, true, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false }
+        };
+
+        public static bool[,] ExpectedGridRule4 =
+        {
+            { false, true, true, false, false },
+            { false, true, true, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false },
+            { false, false, false, false, false }
+        };
+
+        [Test]
+        public void TestRule1()
+        {
+            GameOfLife game = new GameOfLife();
+            Assert.AreEqual(ExpectedGridRule1, game.LifeGenerator(testGridRule1));
+        }
+
+        [Test]
+        public void TestRule2()
+        {
+            GameOfLife game = new GameOfLife();
+            Assert.AreEqual(ExpectedGridRule2, game.LifeGenerator(testGridRule2));
+        }
+
+        [Test]
+        public void TestRule3()
+        {
+            GameOfLife game = new GameOfLife();
+            Assert.AreEqual(ExpectedGridRule3, game.LifeGenerator(testGridRule3));
+        }
+
+        [Test]
+        public void TestRule4()
+        {
+            GameOfLife game = new GameOfLife();
+            Assert.AreEqual(ExpectedGridRule4, game.LifeGenerator(testGridRule4));
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        public bool[,] LifeGenerator(bool[,] start)
+        {
+            bool[,] newGrid = new bool[start.GetLength(0), start.GetLength(1)];
+
+            for (int row = 0; row < start.GetLength(0); row++)
+            {
+                for (int col = 0; col < start.GetLength(1); col++)
+                {
+                    newGrid[row, col] = start[row, col];
+                }
+            }
+
+
+            for (int row = 0; row < start.GetLength(0); row++)
+            {
+                for (int col = 0; col < start.GetLength(1); col++)
+                {
+                    int neighbors = NeighborCount(start, row, col);
+                    if(neighbors < 2 || neighbors > 3)
+                    {
+                        newGrid[row, col] = false;
+                    }
+                    if(!start[row, col] && neighbors == 3)
+                    {
+                        newGrid[row, col] = true;
+                    }
+                }
+            }
+            return newGrid;
+        }
+
+        private int NeighborCount(bool[,] grid, int row, int col)
+        {
+            int count = 0;
+            //row above
+            if(!((row - 1) < 0) && !((col - 1) < 0) && grid[row-1, col-1])
+            {
+                count++;
+            }
+            if (!((row - 1) < 0) && grid[row - 1, col])
+            {
+                count++;
+            }
+            if (!((row - 1) < 0) && !((col + 1) == grid.GetLength(1)) && grid[row - 1, col + 1])
+            {
+                count++;
+            }
+
+            //same row
+            if (!((col - 1) < 0) && grid[row, col - 1])
+            {
+                count++;
+            }
+            if (!((col + 1) == grid.GetLength(1)) && grid[row, col + 1])
+            {
+                count++;
+            }
+
+            //row below
+            if (!((row + 1) == grid.GetLength(0)) && !((col - 1) < 0) && grid[row + 1, col - 1])
+            {
+                count++;
+            }
+            if (!((row + 1) == grid.GetLength(0)) && grid[row + 1, col])
+            {
+                count++;
+            }
+            if (!((row + 1) == grid.GetLength(0)) && !((col + 1) == grid.GetLength(1)) && grid[row + 1, col + 1])
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
