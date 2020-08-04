83e30a4755daf0b2c5ea03925824e733cd8fea6d
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-08-04 17:53:29 -0400

    Added in view

---
+++ GameOfLife/GameOfLifeView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    public class GameOfLifeView
+    {
+        public void Start()
+        {
+            GameOfLife game = new GameOfLife();
+            Random random = new Random();
+            int rows = 49;
+            int cols = 99;
+            bool[,] map = new bool[rows,cols];
+            for(int life = 0; life < 750; life++)
+            {
+                map[random.Next(rows), random.Next(cols)] = true;
+            }
+
+            DateTime start = DateTime.Now;
+            DateTime now = DateTime.Now;
+
+            Console.SetWindowSize(cols+1, rows+1);
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int timeDelay = 150;
+            int generation = 0;
+            int population;
+            int priorPopulation = 0;
+
+            while(true)
+            {
+                if((now-start).Milliseconds > timeDelay)
+                {
+                    population = 0;
+                    //Console.Clear();
+                    generation++;
+                    start = now;
+                    Console.SetCursorPosition(0, 0);
+                    for (int row = 0; row < map.GetLength(0); row++)
+                    {
+                        StringBuilder line = new StringBuilder();
+                        for (int col = 0; col < map.GetLength(1); col++)
+                        {
+                            if (map[row, col])
+                            {
+                                population++;
+                                line.Append("O");
+                            }
+                            else
+                            {
+                                line.Append(" ");
+                            }
+                        }
+                        Console.WriteLine(line);
+                    }
+                    map = game.LifeGenerator(map);
+                    Console.Write($"Population: {population, -10} | Population Direction: {(population-priorPopulation == 0 ? "\u2192" : (population-priorPopulation > 0 ? "\u2191" : "\u2193")), -10} | Generation: {generation, -10}");
+                    priorPopulation = population;
+                }
+                now = DateTime.Now;
+            }
+        }
+    }
+}
