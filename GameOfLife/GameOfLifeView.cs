0bff4f55e6860f00f41598c6f31d0c6b705e6164
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-08-04 19:27:22 -0400

    added in ability to restart the simulation

--- GameOfLife/GameOfLifeView.cs
+++ GameOfLife/GameOfLifeView.cs
@@ -8,73 +8,100 @@ namespace GameOfLife
     {
         public void Start()
         {
-            GameOfLife game = new GameOfLife();
-            Random random = new Random();
-            int rows = 49;
-            int cols = 99;
-            bool[,] map = new bool[rows,cols];
-            for(int life = 0; life < 750; life++)
-            {
-                map[random.Next(rows), random.Next(cols)] = true;
-            }
-
-            DateTime start = DateTime.Now;
-            DateTime now = DateTime.Now;
-
-            Console.SetWindowSize(cols+1, rows+1);
-            Console.CursorVisible = false;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.OutputEncoding = Encoding.Unicode;
-
-
-            int minDelay = 10;
-            int maxDelay = 500;
             int timeDelay = 150;
-            int generation = 0;
-            int population;
-            int priorPopulation = 0;
-
-            while(true)
+            while (true)
             {
-                if((now-start).Milliseconds > timeDelay)
+                GameOfLife game = new GameOfLife();
+                Random random = new Random();
+                int rows = 49;
+                int cols = 99;
+                int initialPopulation = 0;
+                bool[,] map = new bool[rows, cols];
+                for (int life = 0; life < 750; life++)
                 {
-                    population = 0;
-                    //Console.Clear();
-                    generation++;
-                    start = now;
-                    Console.SetCursorPosition(0, 0);
-                    for (int row = 0; row < map.GetLength(0); row++)
+                    int thisRow = random.Next(rows);
+                    int thisCol = random.Next(cols);
+                    if(map[thisRow, thisCol])
                     {
-                        StringBuilder line = new StringBuilder();
-                        for (int col = 0; col < map.GetLength(1); col++)
-                        {
-                            if (map[row, col])
-                            {
-                                population++;
-                                line.Append("O");
-                            }
-                            else
-                            {
-                                line.Append(" ");
-                            }
-                        }
-                        Console.WriteLine(line);
+                        initialPopulation++;
                     }
-                    map = game.LifeGenerator(map);
-                    Console.Write($"Population: {population, -10} | Population Direction: {(population-priorPopulation == 0 ? "\u2192" : (population-priorPopulation > 0 ? "\u2191" : "\u2193")), -10} | Generation: {generation, -10}");
-                    priorPopulation = population;
+                    map[thisRow, thisCol] = true;
                 }
-                CheckSpeedAdjustment(ref timeDelay);
-                if(timeDelay < minDelay)
+
+                DateTime start = DateTime.Now;
+                DateTime now = DateTime.Now;
+
+                Console.SetWindowSize(cols + 1, rows + 3);
+                Console.CursorVisible = false;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.OutputEncoding = Encoding.Unicode;
+
+                int minDelay = 10;
+                int maxDelay = 500;
+                
+                int generation = 0;
+                int population;
+                int priorPopulation = 0;
+
+                while (true)
                 {
-                    timeDelay = minDelay;
+                    if ((now - start).Milliseconds > timeDelay)
+                    {
+                        population = 0;
+                        //Console.Clear();
+                        generation++;
+                        start = now;
+                        Console.SetCursorPosition(0, 0);
+                        for (int row = 0; row < map.GetLength(0); row++)
+                        {
+                            StringBuilder line = new StringBuilder();
+                            for (int col = 0; col < map.GetLength(1); col++)
+                            {
+                                if (map[row, col])
+                                {
+                                    population++;
+                                    line.Append("O");
+                                }
+                                else
+                                {
+                                    line.Append(" ");
+                                }
+                            }
+                            Console.WriteLine(line);
+                        }
+                        map = game.LifeGenerator(map);
+                        Console.WriteLine();
+                        Console.Write($"Population: {population,-10} | Population Direction: {(population - priorPopulation == 0 ? "\u2192" : (population - priorPopulation > 0 ? "\u2191" : "\u2193")),-10} | Generation: {generation,-10}");
+                        priorPopulation = population;
+                    }
+                    CheckSpeedAdjustment(ref timeDelay);
+                    if (timeDelay < minDelay)
+                    {
+                        timeDelay = minDelay;
+                    }
+                    else if (timeDelay > maxDelay)
+                    {
+                        timeDelay = maxDelay;
+                    }
+                    now = DateTime.Now;
+                    if(CheckRestart())
+                    {
+                        break;
+                    }
                 }
-                else if(timeDelay > maxDelay)
-                {
-                    timeDelay = maxDelay;
-                }
-                now = DateTime.Now;
             }
+        }
+
+        private bool CheckRestart()
+        {
+            if(Console.KeyAvailable)
+            {
+                if(Console.ReadKey(true).Key == ConsoleKey.R)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         private void CheckSpeedAdjustment(ref int currentSpeed)
